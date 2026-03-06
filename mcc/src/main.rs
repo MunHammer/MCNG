3f36f647860c14da3d3c309407b23a526d503d53
MunHammer <kyden.j0892@gmail.com>
2026-03-06 15:36:45 +1300

    Added the argument parser It's locked behind the 'cli' feature

--- mcc/src/main.rs
+++ mcc/src/main.rs
@@ -1,4 +1,65 @@
 #![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+
+#[cfg(feature = "cli")]
+mod cli {
+    use clap::{Parser, ValueEnum};
+    use std::path::PathBuf;
+    #[cfg(not(target_os = "windows"))]
+    const DEFAULT_OUT: &str = "a.out";
+    #[cfg(target_os = "windows")]
+    const DEFAULT_OUT: &str = "a.exe";
+    #[derive(Parser)]
+    #[command(version, about, long_about = None)]
+    struct Cli {
+        /// The file to compile
+        #[arg(value_name = "FILE")]
+        file: PathBuf,
+        /// The output file
+        #[arg(short, long, value_name = "FILE",
+            default_value = DEFAULT_OUT)]
+        output: PathBuf,
+        /// The standard to use
+        #[arg(long, value_enum, default_value_t)]
+        std: Std,
+        /// Issue all warnings as required by strict ISO C
+        #[arg(long)]
+        pedantic: bool,
+        /// Treat all warnings as complete errors & blocks compilaion
+        #[arg(long = "Dwarn")]
+        d_errors: bool,
+        /// Add directory to include search path
+        #[arg(short = 'I', value_name = "DIR")]
+        search_dirs: Vec<PathBuf>,
+        /// Predefine a macro
+        #[arg(short = 'D', value_name = "MACRO")]
+        macros: Vec<String>,
+        /// Stop after compilation, do not link
+        #[arg(short)]
+        compile_only: bool,
+    }
+    #[derive(Default, Clone, ValueEnum)]
+    enum Std {
+        #[default]
+        C89,
+        Ansi,
+    }
+    pub fn run() {
+        let cli = Cli::parse();
+        assert!(
+            cli.file.is_file(),
+            "{} is not a valid input file",
+            cli.file.display()
+        );
+        todo!();
+    }
+}
 fn main() {
-    todo!();
+    #[cfg(feature = "cli")]
+    cli::run();
+    #[cfg(not(feature = "cli"))]
+    compile_error!(
+        "This program is not a CLI
+        enable it through:
+        --features cli"
+    );
 }
